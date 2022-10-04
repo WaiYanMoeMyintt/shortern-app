95495a2c442a8c9c83e61ba5d6acdf9f184574ec
WaiYanMoeMyint <wymoemyint4523@gmail.com>
2022-10-04 22:24:26 +0630

    Add Tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontSize:{
+      x2l: '1.6rem',
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
